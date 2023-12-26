d87fb44d640301936e47fc49d74a82f595ebb8b3
adityagoel28 <adityagoel2002@gmail.com>
2023-12-26 19:10:37 +0530

    Created a stack using push operation

--- src/main/java/StackQueue/Main.java
+++ src/main/java/StackQueue/Main.java
@@ -3,5 +3,10 @@ package StackQueue;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Stack Queue Program!");
+
+        Stack<Integer> stack = new Stack<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
     }
 }
